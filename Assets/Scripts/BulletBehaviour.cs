0cf36743ae29cc56c458630826921e20a74a1908
DESKTOP-TDHP4E4\Ettore <ettore.palma94@gmai.com>
2018-01-26 21:22:23 +0100

    Movimento player. Comportamento dei proiettili

---
+++ Assets/Scripts/BulletBehaviour.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletBehaviour : MonoBehaviour {
+
+    public float lifeTime;
+    public float speed;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Movement();
+	}
+
+    void Movement() {
+        if (lifeTime <= 0)
+            Death();
+        else
+        {
+            lifeTime -= Time.deltaTime;
+            transform.position += transform.forward * Time.deltaTime * speed;
+        }
+    }
+
+    void Death() {
+        Destroy(this.gameObject);
+    }
+}
