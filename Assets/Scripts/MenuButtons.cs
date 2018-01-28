0b73f7df8d29d72cf906f8c33bc555f3ecf01d27
DESKTOP-TDHP4E4\Ettore <ettore.palma94@gmai.com>
2018-01-28 13:49:18 +0100

    Aggiunte scene menu e crediti

---
+++ Assets/Scripts/MenuButtons.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MenuButtons : MonoBehaviour {
+
+    Button button;
+
+	// Use this for initialization
+	void Start () {
+        button = GetComponent<Button>();
+        button.onClick.AddListener(Execute);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void Execute() {
+        if (button.name.Equals("Start"))
+        {
+            SceneManager.LoadScene(1);
+        }
+        else if (button.name.Equals("Quit"))
+        {
+            Application.Quit();
+        }
+        else if (button.name.Equals("Credits")) {
+            SceneManager.LoadScene(2);
+        }
+        else if (button.name.Equals("Back")) {
+            SceneManager.LoadScene(0);
+        }
+    }
+}
