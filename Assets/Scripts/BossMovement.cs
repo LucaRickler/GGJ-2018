103427e5293dd43a5576eddc12a27b00866faf3a
Luca Rickler <luca.rickler@gmail.com>
2018-01-28 11:50:43 +0100

    Boss Color

--- Assets/Scripts/BossMovement.cs
+++ Assets/Scripts/BossMovement.cs
@@ -10,32 +10,80 @@ public class BossMovement : MonoBehaviour {
 
 	private Animator animator;
 	public MeshRenderer nucleusRender;
+	public MeshRenderer[] shellRender;
 	[SerializeField]
-	private Material _positive_material;
+	private Material _positive_shell_material;
 	[SerializeField]
-	private Material _negative_material;
+	private Material _negative_shell_material;
+	[SerializeField]
+	private Material _positive_core_material;
+	[SerializeField]
+	private Material _negative_core_material;
+
+	[SerializeField]
+	private float _color_change_time = 20.0f;
+	private float _current_color_timer;
 
 	// Use this for initialization
 	void Start () {
 		animator = GetComponent<Animator> ();
+		if (Random.value < 0.5f) {
+			Debug.Log ("Boss Positive");
+			polarity = Definitions.Polarity.Positive;
+			nucleusRender.material = _positive_core_material;
+			var mats1 = shellRender [0].materials;
+			var mats2 = shellRender [2].materials;
+			mats1 [0] = _positive_shell_material;
+			mats2 [1] = _positive_shell_material;
+			for (int i = 0; i < 6; i++)
+				shellRender [i].materials = (i != 2 ? mats1 : mats2);
+		} else {
+			Debug.Log ("Boss Negative");
+			nucleusRender.material = _negative_core_material;
+			var mats1 = shellRender [0].materials;
+			var mats2 = shellRender [2].materials;
+			mats1 [0] = _negative_shell_material;
+			mats2 [1] = _negative_shell_material;
+			for (int i = 0; i < 6; i++)
+				shellRender [i].materials = (i != 2 ? mats1 : mats2);
+		}
+
+		_current_color_timer = _color_change_time;
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
         if (player != null)
             Following();
+		_current_color_timer -= Time.deltaTime;
+		if (_current_color_timer < 0) {
+			_current_color_timer = _color_change_time;
+			ChangeColor ();
+		}
 
 	}
-
+		
 	private Definitions.Polarity polarity;
 
 	void ChangeColor() {
 		if (polarity == Definitions.Polarity.Negative) {
 			polarity = Definitions.Polarity.Positive;
-			nucleusRender.material = _positive_material;
+			nucleusRender.material = _positive_core_material;
+			var mats1 = shellRender [0].materials;
+			var mats2 = shellRender [2].materials;
+			mats1 [0] = _positive_shell_material;
+			mats2 [1] = _positive_shell_material;
+			for (int i = 0; i < 6; i++)
+				shellRender [i].materials = (i != 2 ? mats1 : mats2);
 		} else {
 			polarity = Definitions.Polarity.Negative;
-			nucleusRender.material = _negative_material;
+			nucleusRender.material = _negative_core_material;
+			var mats1 = shellRender [0].materials;
+			var mats2 = shellRender [2].materials;
+			mats1 [0] = _negative_shell_material;
+			mats2 [1] = _negative_shell_material;
+			for (int i = 0; i < 6; i++)
+				shellRender [i].materials = (i != 2 ? mats1 : mats2);
 		}
 	}
 
